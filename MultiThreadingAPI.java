9ca1d1a0bf15fdc2d3a2e6f409ac5286a5af4593
teixayo <95528418+teixayo@users.noreply.github.com>
2023-04-07 21:12:54 +0330

    Create MultiThreadingAPI.java

---
+++ MultiThreadingAPI.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.IntConsumer;
+
+public class MultiThreadingAPI {
+
+    public static void runAsync(int size, int threads, IntConsumer action) {
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        List<Callable<Void>> tasks = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            final int index = i;
+            tasks.add(() -> {
+                action.accept(index);
+                return null;
+            });
+        }
+        try {
+            executor.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    public static void runAsync(int size, IntConsumer action) {
+        runAsync(size, Runtime.getRuntime().availableProcessors(), action);
+    }
+}
